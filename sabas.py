798edaf04dbd2e2ad96a4fcd2ea3dd3b61e9552c
Gareth Jones <oxidising@gmail.com>
2018-11-11 21:24:34 +0000

    Updated filename handling and added screenshots

--- sabas.py
+++ sabas.py
@@ -42,7 +42,7 @@ class sabas(QMainWindow):
 
 	def __init__(self, parent=None):
 		super(sabas, self).__init__(parent)
-		self.check_sudo()
+		# self.check_sudo()
 		self.process_arguments(sys.argv)
 
 
@@ -217,7 +217,9 @@ class sabas(QMainWindow):
 		'''
 		fstat = os.stat(self.iso_filename)
 
-		file_stat = "Filename : " + self.iso_filename + "\n" \
+		filename = self.iso_filename.split("/")[-1]
+
+		file_stat = "Filename : " + filename + "\n" \
 					"Size : " + self.sabas_obj.convert_size(fstat.st_size) + "\n"
 					
 		
